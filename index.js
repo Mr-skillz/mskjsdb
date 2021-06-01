7bdb3802cb99f494050dc859baab88c15a3d8103
mr-skillz <skillzdboss@gmail.com>
2021-06-01 21:46:22 +0100

    updates

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const db = require('./msk_db')
-db.mskDb.db('test')
+const mydb = require('./msk_db')
+mydb.mskDb.db = 'test';
 
-// db.mskDb.createDb('test');
-db.mskDb.dbInsert('peter', 'test', 'this is a simple test')
+
